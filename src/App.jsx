5fe8ef83c560313a727ec3c7c61e8923388817f7
Miroslavv99 <mbochko99@gmail.com>
2026-03-26 11:24:07 +0200

    refactor: improve project structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-
-  function handleName() {
-    setName("Miroslav");
-  }
-  return (
-    <>
-      <button onClick={handleName}>Click</button>
-      <h1>{name}</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
