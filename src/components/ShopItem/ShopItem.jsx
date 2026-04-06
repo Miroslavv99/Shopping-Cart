98563d05bd7b8bb0ad464969cedcca9a91ee9fe7
Miroslavv99 <mbochko99@gmail.com>
2026-04-06 12:08:12 +0300

    style: stylized product cards

--- src/components/ShopItem/ShopItem.jsx
+++ src/components/ShopItem/ShopItem.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import styles from "./ShopItem.module.css";
-import { useOutletContext } from "react-router";
 
 function ShopItem({ product, addToCart }) {
   const { images, title, price } = product;
@@ -14,10 +13,16 @@ function ShopItem({ product, addToCart }) {
   return (
     <div className={styles.card}>
       <img src={images[0]} alt="img" />
-      <h4>{title}</h4>
-      <span>{price}</span>
-
-      <button onClick={() => addToCart(product)}>ADD +</button>
+      <h4 className={styles.productTitle}>{title}</h4>
+      <div className={styles.productDescription}>
+        <span>${price}</span>
+        <button
+          className={styles.addProduct}
+          onClick={() => addToCart(product)}
+        >
+          To Cart +
+        </button>
+      </div>
     </div>
   );
 }
