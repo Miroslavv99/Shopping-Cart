244943d7f002910ccd9f40efebcc9775a9b55f31
Miroslavv99 <mbochko99@gmail.com>
2026-04-20 11:48:49 +0300

    feat: footer text customized

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import styles from "./Footer.module.css";
 
 function Footer() {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>Moneri © 2026</p>
+      <p>Style starts here.</p>
+    </footer>
+  );
 }
 
 export default Footer;
